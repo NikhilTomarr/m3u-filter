d36626f59db89612cfe27c3ec0476ece29ada9a7
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-09 20:53:26 +0530

    Update tn.php

--- api/tn.php
+++ api/tn.php
@@ -16,7 +16,7 @@ https://otte.live.fly.ww.aiv-cdn.net/iad-nitro/live/clients/dash/enc/yybzgcajse/
 #KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=d0f2e5c39e70c18f29bf77768a1ad89a&key=d6853c51fcf37a18905f0609972395d7
 https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd?
 
-#EXTINF:-1 group-title="IND vs SA" tvg-logo="g", TNT SPORTS
+#EXTINF:-1 group-title="IND vs SA" tvg-logo="g", WILLOW
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=f7bd765bfed600f1cb2658645e936903&key=44b76accbc94ebc6e67e7eba59b88f92
 https://otte.live.fly.ww.aiv-cdn.net/iad-nitro/live/clients/dash/enc/yybzgcajse/out/v1/60337bea559c4e399704e72eaedc8ff7/cenc.mpd?
