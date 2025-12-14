7f1916899b2dc7009383aae1b798552256c115a5
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-14 18:47:25 +0530

    Create new file tn1.php

--- api/tn1.php
+++ api/tn1.php
@@ -8,9 +8,9 @@ https://livetv-push.hotstar.com/dash/live/2002466/sshindiwv/master.mpd??||cookie
 
 #EXTINF:-1 group-title="IND vs SA" tvg-logo="g", English
 #KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=f7bd765bfed600f1cb2658645e936903&key=44b76accbc94ebc6e67e7eba59b88f92
-https://otte.live.fly.ww.aiv-cdn.net/iad-nitro/live/clients/dash/enc/yybzgcajse/out/v1/60337bea559c4e399704e72eaedc8ff7/cenc.mpd?
-https://livetv-push.hotstar.com/dash/live/2002464/sshd1livetvwv/master.mpd?||cookie=hdntl=exp=1765799389~acl=*sshd1livetv*~id=28ac56d9c6a989ab050850e523859d18~data=hdntl~hmac=70f4e086874aa963f99b4e3a9c075186e48063fb162a3871ac1a38ac12c1bccc|||http-origin=https://www.hotstar.com|||http-referer=https://www.hotstar.com/||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)&drmScheme=clearkey&drmLicense=fe7718fbb3fb4ba78c07cc0f578744e6:624e24b1843b459fab0a949609416f0d
+#KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=fe7718fbb3fb4ba78c07cc0f578744e6&key=624e24b1843b459fab0a949609416f0d
+https://livetv-push.hotstar.com/dash/live/2002464/sshd1livetvwv/master.mpd?||cookie=hdntl=exp=1765799389~acl=*sshd1livetv*~id=28ac56d9c6a989ab050850e523859d18~data=hdntl~hmac=70f4e086874aa963f99b4e3a9c075186e48063fb162a3871ac1a38ac12c1bccc|||http-origin=https://www.hotstar.com|||http-referer=https://www.hotstar.com/||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)
+
 #EXTINF:-1 group-title="IND vs SA" tvg-logo="g", TNT SPORTS
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=a93c1cbfdccd23233bac13540c693e7f&key=2f6ab2e6693eb847eff3c9da8f9d01fc
@@ -22,4 +22,4 @@ https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/dash/enc/lsdasbvglv/out/v1/bb
 https://otte.live.fly.ww.aiv-cdn.net/iad-nitro/live/clients/dash/enc/yybzgcajse/out/v1/60337bea559c4e399704e72eaedc8ff7/cenc.mpd?
 
 
-?
+
