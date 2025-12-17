20390770c0e1f79c39117d07001a334f78265e6e
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-17 18:40:59 +0530

    Update tn1.php

--- api/tn1.php
+++ api/tn1.php
@@ -4,22 +4,25 @@ https://fansspot.fun/promo.mp4
 #EXTINF:-1 group-title="IND vs SA" tvg-logo="g", HINDI
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=fe7718fbb3fb4ba78c07cc0f578744e6&key=624e24b1843b459fab0a949609416f0d
-https://livetv-push.hotstar.com/dash/live/2002466/sshindiwv/master.mpd??||cookie=hdntl=exp=1765799781~acl=*sshindi*~id=523edb70e293fdbeb92a6647304d35a1~data=hdntl~hmac=f1195920e981d8b1c1de8c677bd12ed10ab3b8a9b76fa315d69eac9dbe6eb30f||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)|||http-referer=https://www.hotstar.com/||||http-origin=https://www.hotstar.com 
+https://livetv-push.hotstar.com/dash/live/2002466/sshindiwv/master.mpd??||cookie=hdntl=exp=1766035205~acl=*sshindi*~id=07cd83078fdf16170be18c871fe6c29a~data=hdntl~hmac=b8af3333af64b6f8871f383c4c8ded1784730ad36b08df39f3c5f4b8f35d49f0||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)|||http-referer=https://www.hotstar.com/||||http-origin=https://www.hotstar.com 
 
 #EXTINF:-1 group-title="IND vs SA" tvg-logo="g", English
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=fe7718fbb3fb4ba78c07cc0f578744e6&key=624e24b1843b459fab0a949609416f0d
-https://livetv-push.hotstar.com/dash/live/2002464/sshd1livetvwv/master.mpd?||cookie=hdntl=exp=1765799389~acl=*sshd1livetv*~id=28ac56d9c6a989ab050850e523859d18~data=hdntl~hmac=70f4e086874aa963f99b4e3a9c075186e48063fb162a3871ac1a38ac12c1bccc|||http-origin=https://www.hotstar.com|||http-referer=https://www.hotstar.com/||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)
+https://livetv-push.hotstar.com/dash/live/2002464/sshd1livetvwv/master.mpd?||cookie=hdntl=exp=1766035205~acl=*sshd1livetv*~id=1a206a1cd0e5ae41f96f26701a9cfb9e~data=hdntl~hmac=b7e84b6addfc10a3aa868fd71d49e2c669fb54843851c58f2392313e6421fdad|||http-origin=https://www.hotstar.com|||http-referer=https://www.hotstar.com/||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)
 
-#EXTINF:-1 group-title="IND vs SA" tvg-logo="g", TNT SPORTS
+#EXTINF:-1 group-title="IND vs SA" tvg-logo="g", TNT SPORTS 2
 #KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=a93c1cbfdccd23233bac13540c693e7f&key=2f6ab2e6693eb847eff3c9da8f9d01fc
-https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/dash/enc/lsdasbvglv/out/v1/bb548a3626cd4708afbb94a58d71dce9/cenc.mpd
+#KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=d0f2e5c39e70c18f29bf77768a1ad89a&key=d6853c51fcf37a18905f0609972395d7
+https://aba3uy3aaaaaaaamfjv2jyrjdxcj4.otte.live.cf.ww.aiv-cdn.net/sin-nitro/live/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd?
+
+#EXTINF:-1 group-title="IND vs SA" tvg-logo="g", FOX
+https://shotner.u0k.workers.dev/6B6DDmR.m3u8
 
 #EXTINF:-1 group-title="IND vs SA" tvg-logo="g", WILLOW
 #KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=54606447858893d667ddca2f4059dd64&key=10dfcd5a8f6576ef927ba7fa78f67f35
-https://a201aivottlinear-a.akamaihd.net/OTTB/lhr-nitro/clients/dash/enc/ap0kfrjxki/out/v1/525db87abb1d4657a6329900717c5bf2/cenc.mpd
+#KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=199aa69bb1b424a9a14ff9ac5c169f50&key=5927bf3a1cca7d9f8f735560db28e2d0
+https://otte.live.cf.ww.aiv-cdn.net/iad-nitro/live/clients/dash/enc/mwddjgrrzb/out/v1/8f33429098f74d9590bae036af2e6527/cenc.mpd
 
 
 
