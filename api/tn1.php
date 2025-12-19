ce66074621b7417504b1ac16a01b5cc7c9d5b67a
Nikhil Tomar <163639102+NikhilTomarr@users.noreply.github.com>
2025-12-19 19:04:39 +0530

    Update streaming links and add new channels

--- api/tn1.php
+++ api/tn1.php
@@ -4,12 +4,18 @@ https://fansspot.fun/promo.mp4
 #EXTINF:-1 group-title="IND vs SA" tvg-logo="g", HINDI
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=fe7718fbb3fb4ba78c07cc0f578744e6&key=624e24b1843b459fab0a949609416f0d
-https://livetv-push.hotstar.com/dash/live/2002466/sshindiwv/master.mpd??||cookie=hdntl=exp=1766035205~acl=*sshindi*~id=07cd83078fdf16170be18c871fe6c29a~data=hdntl~hmac=b8af3333af64b6f8871f383c4c8ded1784730ad36b08df39f3c5f4b8f35d49f0||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)|||http-referer=https://www.hotstar.com/||||http-origin=https://www.hotstar.com 
+https://live-en.aisports.cc/moviebox/device01/playlist.m3u8
+//https://livetv-push.hotstar.com/dash/live/2002466/sshindiwv/master.mpd??||cookie=hdntl=exp=1766035205~acl=*sshindi*~id=07cd83078fdf16170be18c871fe6c29a~data=hdntl~hmac=b8af3333af64b6f8871f383c4c8ded1784730ad36b08df39f3c5f4b8f35d49f0||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)|||http-referer=https://www.hotstar.com/||||http-origin=https://www.hotstar.com 
 
 #EXTINF:-1 group-title="IND vs SA" tvg-logo="g", English
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=fe7718fbb3fb4ba78c07cc0f578744e6&key=624e24b1843b459fab0a949609416f0d
-https://livetv-push.hotstar.com/dash/live/2002464/sshd1livetvwv/master.mpd?||cookie=hdntl=exp=1766035205~acl=*sshd1livetv*~id=1a206a1cd0e5ae41f96f26701a9cfb9e~data=hdntl~hmac=b7e84b6addfc10a3aa868fd71d49e2c669fb54843851c58f2392313e6421fdad|||http-origin=https://www.hotstar.com|||http-referer=https://www.hotstar.com/||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)
+//https://livetv-push.hotstar.com/dash/live/2002464/sshd1livetvwv/master.mpd?||cookie=hdntl=exp=1766035205~acl=*sshd1livetv*~id=1a206a1cd0e5ae41f96f26701a9cfb9e~data=hdntl~hmac=b7e84b6addfc10a3aa868fd71d49e2c669fb54843851c58f2392313e6421fdad|||http-origin=https://www.hotstar.com|||http-referer=https://www.hotstar.com/||http-user-agent=Hotstar;in.startv.hotstar/25.02.24.8.11169%20(Android/15)
+
+#EXTINF:-1 group-title="IND vs SA" tvg-logo="g", TNT SPORTS 
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=9f51f3dc6313ac8bc668e2d9d1c04dfa&key=74bc63e5a193454a91ca494975db33f9
+https://aba3uy3aaaaaaaamfjv2jyrjdxcj4.otte.live.cf.ww.aiv-cdn.net/sin-nitro/live/dash/enc/f0qvkrra8j/out/v1/f8fa17f087564f51aa4d5c700be43ec4/cenc.mpd?
 
 #EXTINF:-1 group-title="IND vs SA" tvg-logo="g", TNT SPORTS 2
 #KODIPROP:inputstream.adaptive.license_type=clearkey
@@ -21,5 +27,6 @@ https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/dash/enc/f0qvkrra8j/out/v1/f8
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.license_key=https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=e6e5c8033a76eaa14cf5f043c63684c6&key=45a1607cbb02e1a1f673bae2e607bbd4
 https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/jmjjscadmf/out/v1/911c6ff00f1a45258e7944abf338702b/cenc.mpd
+
 
 
